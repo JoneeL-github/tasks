ac2f9cb1ed0d2ff5a393cd8c0cc76d04b5842aaa
JoneeAL <johnj.es@outlook.com>
2023-12-06 10:00:43 +0100

    Update main.js

--- task03/main.js
+++ task03/main.js
@@ -14,10 +14,6 @@
 // OUTPUT:    Search result:  Cow, Founded
 //                     Search result:  Co,  Not found
 
-// const originArray = ['Dog', 'Horse', 'Cow'];
-
-// Array of objects
-
 const animalsArray = ['Dog', 'Horse', 'Cow'];
 
 const newArray = animalsArray.map(function (animal) {
@@ -27,26 +23,18 @@ console.log(newArray);
 
 newArray.push('Cat', 'Sheep');
 console.log(newArray);
-//searching the array for positions
-for (let i = 0; i < newArray.length; i++) {
-  console.log(newArray[i] + ' ' + i);
-}
 
-if (newArray[2] === 'Cow') {
-  console.log('yes');
+//where is cow??
+const indexOfCow = newArray.indexOf('Cow');
+console.log(indexOfCow);
+
+//is cow realy at index 2?
+let animal = newArray[2];
+if (animal === 'Cow') {
+  console.log('Cow Found ');
 } else {
   console.log('no');
 }
 
-let cow = newArray[2];
-if (cow === 'Cow') {
-  console.log('result cow Founded ' + cow);
-} else {
-  console.log('error');
-}
-
-if (cow === 'Co') {
-  console.log('yes' + cow);
-} else {
-  console.log('Co, not Found');
-}
+let find = newArray.find((animal) => animal === 'Co');
+console.log('Search result: Co, Not Found ' + find);
