7d7de46f0cc3f6fc2e00576aff202b49cd907d9d
JoneeAL <johnj.es@outlook.com>
2023-12-08 09:20:18 +0100

    updated code

--- task04-1/main.js
+++ task04-1/main.js
@@ -1,14 +1,12 @@
+//l
+
 // Task4 -1
-
 // Create Class with methods
-
 // Class Flower: Properties:
 //  Type (like Rose), Color, Amount ( how many), inStore ( false/true)
 // Methods:
 // change Amount, change color, change is inStore false/True
-
 // Change color, amount, instore
-
 // OUTPUT: Original situation:
 // Flower { Type: 'Rose', Color: 'Red', Amount: 5, inStore: Yes }
 //       After changes:
@@ -23,15 +21,21 @@ class Flower {
     this.instore = instore;
   }
 
-  getFlower () {
+  getFlower() {
     console.log(`${this.type}, ${this.color} ${this.amount}, ${this.instore}`);
   }
 }
-let myFlower = new Flower('Rose', 'Red', 5, 'Yes');
+let myFlower = new Flower('Rose', 'Red', 2, 'Yes');
 myFlower.getFlower();
-console.log('Original situation ', myFlower );
+console.log('Original situation ', myFlower);
 
-if(myFlower) {
+if (myFlower) {
   myFlower = new Flower('Rose', 'Yellow', 4, 'No');
   console.log('after changes: ', myFlower);
+}
+if(myFlower.amount < 5) {
+  myFlower = new Flower('Rose', 'Red', 2, false);
+  console.log('further changes...  ', myFlower);
+} else {
+  console.log('error');
 }
