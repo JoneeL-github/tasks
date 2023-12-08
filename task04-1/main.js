7d671abd24c7fc05c7df4efaef0b1e91f4e4c116
JoneeAL <johnj.es@outlook.com>
2023-12-08 09:07:20 +0100

    coded a javascript class
    added two objects to use with that class

--- task04-1/main.js
+++ task04-1/main.js
@@ -1,2 +1,37 @@
-let demo = document.getElementById('demo');
-demo.innerText = 'hello javascript';
+// Task4 -1
+
+// Create Class with methods
+
+// Class Flower: Properties:
+//  Type (like Rose), Color, Amount ( how many), inStore ( false/true)
+// Methods:
+// change Amount, change color, change is inStore false/True
+
+// Change color, amount, instore
+
+// OUTPUT: Original situation:
+// Flower { Type: 'Rose', Color: 'Red', Amount: 5, inStore: Yes }
+//       After changes:
+// Flower { Type: Rose , Color: Yellow , Amount: 4 , inStore: No }
+
+//what are class properties??
+class Flower {
+  constructor(type, color, amount, instore) {
+    this.type = type;
+    this.color = color;
+    this.amount = amount;
+    this.instore = instore;
+  }
+
+  getFlower () {
+    console.log(`${this.type}, ${this.color} ${this.amount}, ${this.instore}`);
+  }
+}
+let myFlower = new Flower('Rose', 'Red', 5, 'Yes');
+myFlower.getFlower();
+console.log('Original situation ', myFlower );
+
+if(myFlower) {
+  myFlower = new Flower('Rose', 'Yellow', 4, 'No');
+  console.log('after changes: ', myFlower);
+}
