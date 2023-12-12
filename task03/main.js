37d1c0ee7cae6d6123f49214d3354a2e382fe245
JoneeL <50525883+JoneeL-github@users.noreply.github.com>
2023-12-12 10:24:08 +0100

    Update main.js

--- task03/main.js
+++ task03/main.js
@@ -38,16 +38,3 @@ console.log(
 );
 //---------------------------------------------------------//
 
-//ternary vs if else study below
-let isSnowing = false;
-let wheather;
-
-if(isSnowing == true) {
-  wheather = 'YES it is snowing'
-} else {
-  wheather = 'NO it is not snowing'
-}
-console.log(wheather);
-
-let ternary = isSnowing ? 'Yes it is Snowing' : 'No it is not Snowing';
-console.log(ternary);
