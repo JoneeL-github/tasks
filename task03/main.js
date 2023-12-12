28e0062429f7a267e24b464f1c52fcdd4ecf81ba
JoneeL <50525883+JoneeL-github@users.noreply.github.com>
2023-12-12 10:21:13 +0100

    Update main.js

--- task03/main.js
+++ task03/main.js
@@ -45,9 +45,9 @@ let wheather;
 if(isSnowing == true) {
   wheather = 'YES it is snowing'
 } else {
-  wheather = 'NO it is not currently snowing'
+  wheather = 'NO it is not snowing'
 }
 console.log(wheather);
 
-let ternary = isSnowing ? 'Yes it is Snowing' : 'No it is not currently Snowing';
+let ternary = isSnowing ? 'Yes it is Snowing' : 'No it is not Snowing';
 console.log(ternary);
