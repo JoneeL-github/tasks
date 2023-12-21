9e2877d2d87a1c53695376c3158ca7dbfc6f4a8d
JoneeAL <johnj.es@outlook.com>
2023-12-21 12:28:59 +0100

    alot of changes made

---
+++ task05-B/main.js
@@ -0,0 +1,11 @@
+// The purpose of the task is to create a web page where 3 images are presented using a dropdown list.
+// Component and functionalities are made by JavaScript.
+
+// Download the zip file, extract it and use ready-made partitions there.
+// You find css , html, and 3 images files. Use this in task.
+
+// Use dropdown-list to choose color and then it will change picture. More instructions in PDF-file.
+// Do your Task to GitHub and share it with your teacher.
+let body = document.body;
+console.log(body);
+
