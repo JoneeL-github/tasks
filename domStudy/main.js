5709716138178d33b797202a17f32a8a200bc289
JoneeAL <johnj.es@outlook.com>
2023-12-27 08:42:43 +0100

    made better function

--- domStudy/main.js
+++ domStudy/main.js
@@ -1,11 +1,10 @@
 // l
-
-function setImage() {
-  var displayImage = document.getElementById("displayImage");
-  displayImage.src = this.value; //switch between 3 values
-  return false;
+//the NEW function
+function displaySelected() {
+  let ImageList = document.getElementById("ImageList");
+  let imageArea = document.getElementById("imageArea");
+  // Set the content of the image area to the selected value from the dropdown
+  imageArea.src = ImageList.value;
 }
-//selection tag id imageList
-document.getElementById("ImageList").onchange = setImage;
 
 // Reference Vs Value In JavaScript ?? look this up next time
