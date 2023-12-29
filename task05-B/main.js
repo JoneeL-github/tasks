f10562af141b5fd1e272269e4ba67ad7626ee1fd
JoneeAL <johnj.es@outlook.com>
2023-12-29 07:35:02 +0100

    updated removed unused code

--- task05-B/main.js
+++ task05-B/main.js
@@ -3,31 +3,18 @@
 var ImageList = document.getElementById("ImageList");
 var imageArea = document.getElementById("imageArea");
 var displayText = document.getElementById("displayText");
-function displaySelected() {
-  // Set the content of the image area to the selected value from the dropdown
+// Set the content of the image area to the selected value from the dropdown
+function displaySelectedValue() {
   imageArea.src = ImageList.value;
 }
-
 console.log(ImageList);
-
+console.log(imageArea);
+//simulate clicking on image with text function
 function text() {
-  var displayText = document.getElementById("displayText");
-  var text = 'No need to click picture \n';
+  var text = "No need to click picture \n";
   displayText.innerText += text;
 }
 
-function displayText() {
-  if (ImageList.value === "yellow.jpg") {
-    console.log("yes");
-    displayText.innerText = "yes";
-  } else {
-    console.log("no");
-    displayText.style = "block";
-    displayText.innerText = "no";
-  }
-}
-
-var imageArea = document.getElementById("imageArea");
 imageArea.addEventListener("mouseover", function () {
   displayText.style.marginTop = "50px";
   displayText.style.display = "block";
